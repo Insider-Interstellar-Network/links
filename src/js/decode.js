b2d8cc37bd4e4bfa000ae100a3c112c35bc2cf80
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-21 10:06:03 -0400

    Update decode.js

--- src/js/decode.js
+++ src/js/decode.js
@@ -1,5 +1,5 @@
 const keys = [
-  {"num" : "1", "key" : "aHR0cHM6Ly92cjJ4NGstODA4MC5jc2IuYXBwLw=="},
+  {num : "1", key : "aHR0cHM6Ly92cjJ4NGstODA4MC5jc2IuYXBwLw=="},
 ]
 function decode(num) {
   keys.forEach((key) => {
