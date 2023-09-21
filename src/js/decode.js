73f336ef7d61ca29f633a592672dbc2f8d4a6175
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2023-09-21 08:00:50 -0400

    Update decode.js

--- src/js/decode.js
+++ src/js/decode.js
@@ -1,8 +1,4 @@
-function decode(key, key2) {
+function decode(key) {
   var step1 = atob(key);
-  var step2 = atob(step1);
-  var step3 = atob(key2);
-  var step4 = btoa(step2 + step3);
-  var step5 = atob(step4);
-  window.location.href = step5;
+  window.location.href = step1;
 };
